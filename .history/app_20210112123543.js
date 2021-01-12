1dd31c820dad14c6faeb453f4abd79d5f78ab587
umarrg <umarabubakarfta@gmail.com>
2021-01-12 12:41:41 +0100

    added mongodb

---
+++ .history/app_20210112123543.js
@@ -0,0 +1,34 @@
+const { json } = require('express');
+const express = require('express');
+const mongodb = 'mongodb+srv://umarabox:<secret123>@cluster0.t0h3t.mongodb.net/<aboxcrud>?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+mongoose.connect(mongodb).then(()=> console.log('we are connected to mongodb'))
+
+const app = express();
+
+
+app.set('view engine', 'ejs' );
+
+app.get('/', (req, res) => {
+
+    const items =  [
+       {name: 'Mobile phone', price: 1000},
+       {name: 'Pencil', price: 20},
+       {name: 'Hanckerchef', price: 300}
+    ];
+    res.render('index', {items: []});
+});
+
+app.get('/additem', (req, res) => {
+    res.render('additem');
+});
+app.use((req, res) => {
+    res.render('error');
+});
+
+
+
+
+app.listen(4000, ()=> {
+    console.log('our app is running on port 3000');
+})
